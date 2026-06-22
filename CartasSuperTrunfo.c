59e237bd17501fa30eeb60696b1edb1394e27792
Ketlyn0410 <ketlyn.gela@hotmail.com>
2026-06-22 19:09:37 -0300

    adicionei o PIB E Densidade

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -11,7 +11,9 @@ int populacao1 = 100000;
 float areaKm2_1 = 1000.5;
 float pibPerCapita1 = 10000.5;
 int pontosTuristicos1 = 100;
-
+     float densidadePopulacional1 = populacao1 / areaKm2_1;
+float pibPerCapita1 = pibPerCapita1 / populacao1;
+      
 // Carta 2
 char estado2[] = "Ceara";
 char codigoCarta2[] = "A02";
@@ -20,7 +22,9 @@ int populacao2 = 2428678;
 float areaKm2_2 = 312.35;
 float pib2 = 73.4;
 int pontosTuristicos2 = 150;
-
+float densidadePopulacional2 = populacao2 / areaKm2_2;
+float pibPerCapita2 = pib2 / populacao2;
+      
    printf("Codigo da Carta 1: %s\n", codigoCarta1);
    printf("Codigo da Carta 2: %s\n", codigoCarta2);
            printf("=== CARTA 1 ===\n");
