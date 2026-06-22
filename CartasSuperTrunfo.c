07459bac20005f1b36728f6611c66d3358c8d907
Ketlyn0410 <ketlyn.gela@hotmail.com>
2026-06-22 14:58:08 +0000

    Criei as Cartas brasil e Ceara!01

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -2,41 +2,36 @@
 // Desafio Super Trunfo - Países
 // Tema 1 - Cadastro das cartas CARTAS 01
    int main (){
-      char estado[] = "Brasil";
-      char codigoCarta[] = "A01";
-      char nomeCidade[] = "Curitiba";
-        int populacao = 1000000;
-        float areaKm2 = 1000.5;
-        float pibPerCapita = 10000.5;
-        int pontosTuristicos = 100;
-         // Carta 2
-         char estado [] = "Ceará";
-         char codigoCarta [] = "A02";
-         char nomeCidade [] = "Fortaleza";
-         int populacao = 2000000;
-         float areaKm2 = 2000.5;
-         float pibPerCapita = 20000.5;
-         int pontosTuristicos = 200;
-           // Exibição da Carta 1
-           printf("Carta 1:\n");
-           printf("Estado: %c\n", "Estado01");
-           printf("Codigo: %s\n", "codigo01");
-           printf("Nome da Cidade: %s\n", "cidade01");
-           printf("Populacao: %d\n", populacao);
-           printf("Area: %.2f km²\n", "area");
-           printf("PIB: %.2f bilhoes de reais\n", "pib1");
-           printf("Numero de Pontos Turisticos: %d\n\n", "pontos01");
-           // Exibição da Carta 2
-           printf("Carta 2:\n");
-           printf("Estado: %c\n", "Estado02");
-           printf("Codigo: %s\n", "codigo02");
-           printf("Nome da Cidade: %s\n", "cidade02");
-           printf("Populacao: %d\n", populacao);
-           printf("Area: %.2f km²\n", "area");
-           printf("PIB: %.2f bilhoes de reais\n", "pib2");
-           printf("Numero de Pontos Turisticos: %d\n\n", "pontos02");
 
-return 0;
+     // Carta 1
+char estado1[] = "Brasil";
+char codigoCarta1[] = "A01";
+char nomeCidade1[] = "Curitiba";
+int populacao1 = 100000;
+float areaKm2_1 = 1000.5;
+float pibPerCapita1 = 10000.5;
+int pontosTuristicos1 = 100;
+
+// Carta 2
+char estado2[] = "Ceara";
+char codigoCarta2[] = "A02";
+char nomeCidade2[] = "Fortaleza";
+int populacao2 = 2428678;
+float areaKm2_2 = 312.35;
+float pib2 = 73.4;
+int pontosTuristicos2 = 150;
+
+   printf("Codigo da Carta 1: %s\n", codigoCarta1);
+   printf("Codigo da Carta 2: %s\n", codigoCarta2);
+           printf("=== CARTA 1 ===\n");
+printf("Estado: %s\n", estado1);
+printf("Codigo: %s\n", codigoCarta1);
+printf("Cidade: %s\n", nomeCidade1);
+
+printf("\n=== CARTA 2 ===\n");
+printf("Estado: %s\n", estado2);
+printf("Codigo: %s\n", codigoCarta2);
+printf("Cidade: %s\n", nomeCidade2);
 
    }
    
